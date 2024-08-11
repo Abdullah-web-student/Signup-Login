a67be3e406ce237bf36e3d90645520d7cc36c4d2
Abdullah-web-student <abdullah2401a@aptechsite.net>
2024-08-11 03:31:34 -0700

    First Time

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#login').click(function() {
+        $('.login').css("background-color", "#051F20")
+        $('.signup').css("background-color", "rgba(0, 0, 0, 0.30)")
+    })
+
+    $('#signup').click(function() {
+        $('.signup').css("background-color", "#051F20")
+        $('.login').css("background-color", "rgba(0, 0, 0, 0.30)")
+    })
+})
